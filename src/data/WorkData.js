51eb87b5fc4318c1610ef12ba064823727ef5795
Abhishek Singh Rana <abhisingh.72340@gmail.com>
2023-03-16 15:10:01 +0530

    Add files via upload

---
+++ src/data/WorkData.js
@@ -0,0 +1,41 @@
+export const Work = [
+    {
+        id:1,
+        name:"Emart ",
+        description:"It is a e-commerce build on top of the React JS, with dummy.JSON as backend",
+        tags:["react","dummyjson"],
+        
+        demo:"https://emart-1.netlify.app/",
+        github:"https://github.com/abhishek72340/Emart"
+    },
+    {
+        id:2,
+        name:"Extension",
+        description:"A chrome extension build in ReactJS. With connect with local storage",
+        tags:["react","mockbee"],
+        
+        demo:"https://pocket-chrome-extension.netlify.app/",
+        github:"https://github.com/abhishek72340/Pocket-Chrome-Extension"
+        
+    },
+    {
+        id:3,
+        name:"Service-App-UI",
+        description:"A UI build in ReactJS.",
+        tags:["react","UI/UX","design"],
+        
+        demo:"https://service-app-clone.netlify.app",
+        github:"https://github.com/abhishek72340/React-App--service-clone"
+    },
+  {
+        id:5,
+        name:"Minion-Speak-AppI",
+        description:"A minion-speak-app build using javascript.",
+        tags:["html & css","Javascript"],
+        
+        demo:"https://minion1.netlify.app",
+        github:"https://github.com/Asuraking1n/wish-ui"
+        
+    },
+
+] 
