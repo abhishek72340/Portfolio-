6d655676159d7cf8f01a536b5f2acb5fb0ab7f89
abhishek72340 <abhisingh.72340@gmail.com>
2023-10-06 18:57:33 +0530

    update project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./globalStyles";
 import { lightTheme } from "./components/Themes";
-import { Route, Switch, useLocation } from "react-router"
+import { Route, Switch, useLocation } from "react-router";
 import Main from "./components/Main";
 import AboutPage from "./components/AboutPage";
 import BlogPage from "./components/BlogPage";
 import WorkPage from "./components/WorkPage";
 import MySkillsPage from "./components/MySkillsPage";
 import { AnimatePresence } from "framer-motion";
+// abhishek
 // import SoundBar from "./subComponents/SoundBar";
 function App() {
   const location = useLocation();
